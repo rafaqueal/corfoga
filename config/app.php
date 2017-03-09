7a71159734477a1905c8d9d780c974af9cf6ad5f
rafaqueal <rafa.quesada.al@hotmail.com>
2017-03-09 09:51:31 -0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://corfogaweb.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
